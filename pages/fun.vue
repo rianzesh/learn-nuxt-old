ef2fd47e84825cdb5cf0db5c9c749a837addf020
Rahmat Rians <rahmatriansyah57@gmail.com>
2021-11-05 19:40:33 +0700

    ma first commit yoo!

---
+++ pages/fun.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Yoo! dude</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="bg-red-300">
+    <div class="x-parallax-img fixed z-0">
+      <img class="h-full" src="~/assets/images/parallax2.jpg" />
+    </div>
+    <div class="md:px-16 grid grid-cols-2 h-screen bg-blue-400 relative z-1">
+      <div class="bg-yellow-300 p-3 place-self-center">
+        <img class="h-96" src="~/assets/images/osamu.png" />
+      </div>
+      <div class="bg-green-500 justify-self-start place-self-center">
+        <h1 class="text-2xl font-poppinsBold">Hello, I'm</h1>
+        <h1 class="text-6xl font-poppinsBold">Rahmat Riansyah</h1>
+        <p class="mt-5 text-2xl">
+          A Syntax Enthusiast and Photography Addict, <br />
+          but <b>Mobile Developer</b> Wanna be!
+        </p>
+      </div>
+    </div>
+    <div class="grid grid-cols-2 h-96 bg-pink-200 relative z-1"></div>
+    <div class="md:px-10 grid grid-cols-2 h-96 relative z-1">
+      <div class="bg-yellow-300 p-3 place-self-center">
+        <h1 class="text-2xl font-poppinsBold">Let's, Talking</h1>
+        <h1 class="text-6xl font-poppinsBold">About Me</h1>
+        <p class="mt-3 text-2xl">Tak Kenal, Maka Kenalan</p>
+      </div>
+      <div class="bg-transparent justify-self-start place-self-center">
+        <h1 class="text-2xl font-poppinsBold">Yoo!</h1>
+        <p class="bg-white">
+          Currently, i’m Informatics student at Mercu Buana University who like
+          challenges and learn something new, that’s why i’m interested in
+          programming field especially Mobile Development. I have others hobby
+          such as Design, Photography, Play Badminton and also Draw Cartoon
+          Character.
+          <br />
+          <br />
+          That you know?! i have a special skill is <b>LeftHanded</b>, yap i
+          always do everthing with my left hand and feet like use racket, use
+          pencil, kick ball etc. Except to eat, i must use my right hand.
+        </p>
+      </div>
+    </div>
+    <div class="grid grid-cols-2 h-96 bg-pink-200 relative z-1"></div>
+    <div class="grid grid-cols-2 h-screen bg-blue-400 relative z-1">
+      <div class="bg-yellow-300 p-3 place-self-center">
+        <img class="h-96" src="~/assets/images/osamu.png" />
+      </div>
+      <div class="bg-green-500 place-self-center">
+        <h1 class="text-4xl font-poppinsBold">Hello, I'm Rian</h1>
+        <p>
+          My fullname is <b>Rahmat Riansyah</b>, i love do coding in everyday. i
+          think programming is my best hobby in my life.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
