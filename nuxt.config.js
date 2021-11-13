55e68cd04eec0ae598aa04629ee3aec494266835
Rahmat Rians <rahmatriansyah57@gmail.com>
2021-11-13 10:17:10 +0700

    change title app

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '-Night Fury',
+    title: 'Night Fury!',
     htmlAttrs: {
       lang: 'en'
     },
