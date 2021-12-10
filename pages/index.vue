c2fb06ab8ff5ce838c6c5a97d80ab0cd0ef04d21
Rahmat Rians <rahmatriansyah57@gmail.com>
2021-12-11 01:34:42 +0700

    fixing about me, portofolio sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="bg-red-300">
     <div class="x-parallax-img fixed z-0">
-      <img class="h-full" src="~/assets/images/parallax2.jpg" />
+      <img class="h-screen md:h-full" src="~/assets/images/parallax2.jpg" />
     </div>
 
     <section>
-      <div class="md:px-16 grid grid-cols-2 h-screen bg-black relative z-1">
-        <div class="bg-yellow-0 place-self-center">
-          <img class="rounded-full h-96" src="~/assets/images/ME.jpg" />
+      <div
+        class="md:px-16 md:grid md:grid-cols-2 h-screen bg-black relative z-1"
+      >
+        <div class="bg-yellow-0 pt-32 md:place-self-center">
+          <img
+            class="rounded-full mx-auto h-60 md:h-96"
+            src="~/assets/images/ME.jpg"
+          />
         </div>
-        <div class="text-white bg-green-0 justify-self-start place-self-center">
-          <h1 class="text-2xl font-poppinsBold">Hello, I'm</h1>
-          <h1 class="text-6xl font-poppinsBold text-x-secondary-opt1">
+        <div
+          class="
+            text-white
+            bg-green-0
+            text-center
+            mt-14
+            md:mt-0 md:text-left md:justify-self-start
+            place-self-center
+          "
+        >
+          <h1 class="text-96 md:text-2xl font-poppinsBold">Hello, I'm</h1>
+          <h1
+            class="text-3xl md:text-6xl font-poppinsBold text-x-secondary-opt1"
+          >
             Rahmat Riansyah
           </h1>
-          <p class="mt-5 text-2xl">
+          <p class="mt-3 md:mt-5 text-96 md:text-2xl">
             A Design Enthusiast and Photography Addict, <br />
             but <b>Mobile Developer</b> Wanna be!
           </p>
@@ -24,26 +40,67 @@
 
     <section>
       <div class="grid grid-cols-2 h-96 bg-black relative z-1"></div>
-      <div class="md:px-10 grid grid-cols-2 h-96 relative z-1">
-        <div class="p-3 place-self-center">
-          <h1 class="text-white text-2xl font-poppinsBold">Let's, Talking</h1>
+      <div
+        class="
+          md:px-10 md:grid
+          grid-cols-2
+          h-screen
+          md:h-96
+          relative
+          z-1
+          mt-40
+          md:mt-0
+        "
+      >
+        <div class="mx-5 md:mx-0 md:p-3 place-self-center">
+          <h1 class="text-white text-xl md:text-2xl font-poppinsBold">
+            Let's, Talking
+          </h1>
           <div class="bg-x-secondary">
-            <h1 class="text-6xl font-monumentExtendedBold">About Me</h1>
-            <p class="mt-3 text-xl">Tak Kenal, Maka Kenalan</p>
+            <h1 class="text-4xl md:text-6xl font-monumentExtendedBold">
+              About Me
+            </h1>
+            <p class="md:mt-3 text-xl">Tak Kenal, Maka Kenalan</p>
           </div>
         </div>
-        <div class="bg-transparent justify-self-start place-self-center">
-          <h1 class="text-2xl bg-x-secondary inline-block font-poppinsBold">
+        <div
+          class="
+            mx-5
+            mt-10
+            md:mt-0 md:mx-0 md:mr-20
+            justify-self-center
+            md:justify-self-start
+            place-self-center
+          "
+        >
+          <h1
+            class="
+              text-2xl
+              bg-x-secondary
+              inline-block
+              font-poppinsBold
+              mt-10
+              md:mt-0
+            "
+          >
             Yoo!
           </h1>
-          <p class="bg-black text-x-secondary-opt1 inline-block">
+          <p class="bg-black text-x-secondary-opt1 text-xl inline-block">
             Currently, i’m Informatics student at Mercu Buana University who
             like challenges and learn something new, that’s why i’m interested
             in programming field especially Mobile Development. I have others
             hobby such as Design, Photography, Play Badminton and also Draw
             Cartoon Character.
           </p>
-          <p class="mt-2 bg-black text-x-secondary-opt1 inline-block">
+          <p
+            class="
+              mt-5
+              md:mt-2
+              bg-black
+              text-x-secondary-opt1 text-xl
+              inline-block
+            "
+          >
             Did you know?! i have a special skill is <b>LeftHanded</b>, yap i
             always do everthing with my left hand and feet like use racket, use
             pencil, kick ball etc. Except to eat, i must use my right hand.
@@ -56,21 +113,40 @@
     </section>
 
     <section>
-      <div class="bg-black py-40 grid mx-auto relative z-1">
-        <div class="p-3 text-center place-self-center flex mb-28">
-          <div class="">
-            <h1 class="text-white text-2xl font-poppinsBold">
-              Experience Check
+      <div class="bg-black md:py-40 grid mx-auto relative z-1">
+        <div
+          class="
+            mx-5
+            md:mx-0 md:p-3
+            text-center
+            place-self-center
+            mb-20
+            md:mb-28
+          "
+        >
+          <h1 class="text-white text-xl md:text-2xl font-poppinsBold">
+            Experience Check
+          </h1>
+          <div class="bg-x-secondary">
+            <h1 class="text-4xl md:text-6xl font-monumentExtendedBold">
+              Portofolio
             </h1>
-            <div class="bg-x-secondary">
-              <h1 class="text-6xl font-monumentExtendedBold">Portofolio</h1>
-              <p class="mt-3 text-xl">Udah Ngapain Aja?</p>
-            </div>
+            <p class="md:mt-3 text-xl">Udah Ngapain Aja?</p>
           </div>
         </div>
-        <div class="container mx-auto justify-center grid gap-y-8 grid-cols-3">
+        <div
+          class="
+            container
+            mx-auto
+            justify-center
+            grid
+            gap-y-8
+            grid-cols-1
+            md:grid-cols-3
+          "
+        >
           <!-- Card -->
-          <div class="shadow-md place-self-center" style="width: 450px">
+          <div class="shadow-md place-self-center w-10/12 md:full">
             <img class="h-full" src="~/assets/images/parallax2.jpg" />
             <div class="py-4 grid grid-cols-3 bg-gray-800 text-white">
               <div class="col-span-2 place-self-start ml-5 my-auto space-y-1">
@@ -88,7 +164,7 @@
           </div>
           <!-- Card -->
           <!-- Card -->
-          <div class="shadow-md place-self-center" style="width: 450px">
+          <div class="shadow-md place-self-center w-10/12">
             <img class="h-full" src="~/assets/images/parallax2.jpg" />
             <div class="py-4 grid grid-cols-3 bg-gray-800 text-white">
               <div class="col-span-2 place-self-start ml-5 my-auto space-y-1">
@@ -106,7 +182,7 @@
           </div>
           <!-- Card -->
           <!-- Card -->
-          <div class="shadow-md place-self-center" style="width: 450px">
+          <div class="shadow-md place-self-center w-10/12">
             <img class="h-full" src="~/assets/images/parallax2.jpg" />
             <div class="py-4 grid grid-cols-3 bg-gray-800 text-white">
               <div class="col-span-2 place-self-start ml-5 my-auto space-y-1">
@@ -124,7 +200,7 @@
           </div>
           <!-- Card -->
           <!-- Card -->
-          <div class="shadow-md place-self-center" style="width: 450px">
+          <div class="shadow-md place-self-center w-10/12">
             <img class="h-full" src="~/assets/images/parallax2.jpg" />
             <div class="py-4 grid grid-cols-3 bg-gray-800 text-white">
               <div class="col-span-2 place-self-start ml-5 my-auto space-y-1">
@@ -142,7 +218,7 @@
           </div>
           <!-- Card -->
           <!-- Card -->
-          <div class="shadow-md place-self-center" style="width: 450px">
+          <div class="shadow-md place-self-center w-10/12">
             <img class="h-full" src="~/assets/images/parallax2.jpg" />
             <div class="py-4 grid grid-cols-3 bg-gray-800 text-white">
               <div class="col-span-2 place-self-start ml-5 my-auto space-y-1">
@@ -160,7 +236,7 @@
           </div>
           <!-- Card -->
           <!-- Card -->
-          <div class="shadow-md place-self-center" style="width: 450px">
+          <div class="shadow-md place-self-center w-10/12">
             <img class="h-full" src="~/assets/images/parallax2.jpg" />
             <div class="py-4 grid grid-cols-3 bg-gray-800 text-white">
               <div class="col-span-2 place-self-start ml-5 my-auto space-y-1">
@@ -196,64 +272,173 @@
         "
       >
         <div class="bg-transparent justify-self-start place-self-center">
-          <div class="grid grid-cols-3 bf-blue-200 space-x-3">
+          <div class="grid grid-cols-3 space-x-3">
+            <div class="flex flex-col col-span-2 space-y-3">
+              <div
+                class="
+                  border-2
+                  rounded-sm
+                  border-x-secondary-opt1
+                  bg-opacity-0
+                  backdrop-filter backdrop-blur-lg
+                  group
+                  hover:bg-x-secondary-opt1
+                "
+                @mouseover="upHere = true"
+                @mouseleave="upHere = false"
+              >
+                <div class="grid grid-cols-4">
+                  <h1
+                    class="
+                      justify-self-start
+                      text-5xl
+                      ml-10
+                      mt-10
+                      col-span-2
+                      text-x-secondary-opt1
+                      font-monumentExtendedBold
+                      group-hover:text-black
+                    "
+                  >
+                    03
+                  </h1>
+                  <p
+                    class="
+                      justify-self-end
+                      mr-10
+                      my-10
+                      col-span-2
+                      text-x-secondary-opt1
+                      group-hover:text-black
+                    "
+                  >
+                    November 21. 2019
+                  </p>
+                  <h3
+                    class="
+                      text-xl
+                      ml-10
+                      my-10
+                      col-span-3
+                      text-x-secondary-opt1
+                      font-poppinsBold
+                      group-hover:text-black
+                    "
+                  >
+                    CSIC Competition at UMN
+                  </h3>
+                  <img
+                    class="mr-10 mt-10"
+                    v-show="upHere"
+                    src="~/assets/icons/arrow-right.svg"
+                    alt="arrow-right"
+                  />
+                </div>
+              </div>
+              <div
+                class="
+                  border-2
+                  rounded-sm
+                  border-x-secondary-opt1
+                  bg-opacity-0
+                  backdrop-filter backdrop-blur-lg
+                  group
+                  hover:bg-x-secondary-opt1
+                "
+              >
+                <div class="grid grid-cols-4">
+                  <h1
+                    class="
+                      justify-self-start
+                      text-5xl
+                      m-10
+                      col-span-2
+                      text-x-secondary-opt1
+                      font-monumentExtendedBold
+                      group-hover:text-black
+                    "
+                  >
+                    03
+                  </h1>
+                  <p
+                    class="
+                      justify-self-end
+                      m-10
+                      col-span-2
+                      text-x-secondary-opt1
+                      group-hover:text-black
+                    "
+                  >
+                    November 21. 2019
+                  </p>
+                  <h3
+                    class="
+                      justify-self-start
+                      text-xl
+                      m-10
+                      col-span-3
+                      text-x-secondary-opt1
+                      font-poppinsBold
+                      group-hover:text-black
+                    "
+                  >
+                    CSIC Competition at UMN
+                  </h3>
+                </div>
+              </div>
+            </div>
             <div
               class="
                 border-2
                 rounded-sm
                 border-x-secondary-opt1
+                backdrop-filter backdrop-blur-sm
                 bg-opacity-0
-                col-span-2
+                group
+                hover:bg-x-secondary-opt1
               "
             >
-              <div class="grid grid-cols-4">
+              <div class="grid">
                 <h1
                   class="
-                    justify-self-start
-                    text-5xl
+                    text-5xl text-x-secondary-opt1
                     m-10
-                    col-span-2
-                    text-x-secondary-opt1
                     font-monumentExtendedBold
+                    group-hover:text-black
                   "
                 >
                   03
                 </h1>
                 <p
-                  class="justify-self-end m-10 col-span-2 text-x-secondary-opt1"
+                  class="
+                    text-x-secondary-opt1
+                    m-10
+                    group-hover:text-black
+                    justify-self-end
+                  "
                 >
                   November 21. 2019
                 </p>
                 <h3
                   class="
-                    justify-self-start
-                    text-xl
-                    m-10
-                    col-span-3
-                    text-x-secondary-opt1
+                    text-xl text-x-secondary-opt1
                     font-poppinsBold
+                    inset-x-0
+                    m-10
+                    bottom-0
+                    group-hover:text-black
                   "
                 >
                   CSIC Competition at UMN
                 </h3>
               </div>
             </div>
-            <div
-              class="border-2 rounded-sm border-x-secondary-opt1 bg-opacity-0"
-            >
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-                quidem nesciunt sequi illum eum, deserunt labore, veritatis nisi
-                nostrum ex saepe laborum non assumenda perferendis impedit sit
-                est eos hic!
-              </p>
-            </div>
           </div>
         </div>
         <div class="p-3 place-self-center">
           <h1 class="text-white text-2xl font-poppinsBold">Time to Flex my</h1>
           <div class="bg-x-secondary">
-            <h1 class="text-6xl font-monumentExtendedBold">champion</h1>
+            <h1 class="text-6xl font-monumentExtendedBold">Champion</h1>
             <p class="mt-3 text-xl">Berani Unjuk Gigi!</p>
           </div>
         </div>
@@ -264,5 +449,11 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      upHere: false,
+    };
+  },
+};
 </script>
